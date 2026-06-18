b0123a5839d85d08aeac04b25773ce88b9e835f8
TameemMubarak <m.ansarithameem004@gmail.com>
2026-06-18 08:21:00 +0530

    Update Bookings.jsx

--- front-end/src/bookings/Bookings.jsx
+++ front-end/src/bookings/Bookings.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../components/Navbar";
-
+// bookings component
 function Bookings() {
   const [bookings, setBookings] = useState([]);
   const navigate = useNavigate();
